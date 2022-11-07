716b8832e6dbde0b86dc0e7c6b32bb239e8d3e6a
Yuri Shornikov <dbonlyfun@gmail.com>
2022-11-07 16:43:21 +0300

    1 2 3 4-

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.list_students = {}
 
     def rate_lec(self, lecture, course, grade):
         if isinstance(lecture, Lecturer) and course in lecture.courses_attached and course in self.courses_in_progress:
@@ -19,16 +20,26 @@ class Student:
     def _average_rate(self):
         sum_grade = 0
         sum_element = 0
+        average_rate = 0
         for course, grades in self.grades.items():
             for grade in grades:
                 sum_element += 1
                 sum_grade += grade
-        return float("{:.1f}".format(sum_grade / sum_element))
+            average_rate = float("{:.1f}".format(sum_grade / sum_element))
+        return average_rate
 
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self._average_rate()}\n' \
                f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' \
                f'Завершенные курсы: {", ".join(self.finished_courses)}'
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student')
+            return
+        return self._average_rate() < other._average_rate()
+
+    def list_students(self):
 
 class Mentor:
     def __init__(self, name, surname):
@@ -44,15 +55,24 @@ class Lecturer(Mentor):
         self.surname = surname
         self.courses_attached = []
         self.grades = {}
+        self.list_lecturers = {}
 
     def _average_rate(self):
         sum_grade = 0
         sum_element = 0
+        average_rate = 0
         for course, grades in self.grades.items():
             for grade in grades:
                 sum_element += 1
                 sum_grade += grade
-        return float("{:.1f}".format(sum_grade / sum_element))
+            average_rate = float("{:.1f}".format(sum_grade / sum_element))
+        return average_rate
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer')
+            return
+        return self._average_rate() < other._average_rate()
 
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\n' \
@@ -73,30 +93,54 @@ class Reviewer(Mentor):
         return f'Имя: {self.name}\n' \
                f'Фамилия: {self.surname}'
 
+def all_average_students(students, grades):
 
+student_1 = Student('Ruoy', 'Eman', 'your_gender')
+student_1.courses_in_progress += ['Python']
+student_1.courses_in_progress += ['C+']
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+student_2 = Student('Jon', 'Sigal', 'man')
+student_2.courses_in_progress += ['Python']
 
-best_student.courses_in_progress += ['C+']
+reviewer_1 = Reviewer('Some', 'Buddy')
+reviewer_1.courses_attached += ['Python']
 
-cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+reviewer_2 = Reviewer('Snape', 'Severus')
+reviewer_2.courses_attached += ['C+']
 
-best_lecture = Lecturer('Ilon', 'Mask')
-best_lecture.courses_attached += ['C+']
+lecture_1 = Lecturer('Ilon', 'Mask')
+lecture_1.courses_attached += ['C+']
+lecture_2 = Lecturer('James', 'Gosling')
+lecture_2.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 10)
 
-best_student.rate_lec(best_lecture, 'C+', 10)
-best_student.rate_lec(best_lecture, 'C+', 5)
-best_student.rate_lec(best_lecture, 'C+', 0)
+reviewer_1.rate_hw(student_2, 'Python', 5)
+reviewer_1.rate_hw(student_2, 'Python', 3)
 
-print(best_lecture.grades)
-print(best_student.grades)
+student_1.rate_lec(lecture_1, 'C+', 10)
+student_1.rate_lec(lecture_1, 'C+', 5)
+student_1.rate_lec(lecture_1, 'C+', 0)
 
-print(cool_mentor)
-print(best_lecture)
-print(best_student)
+student_2.rate_lec(lecture_2, 'Python', 2)
+student_2.rate_lec(lecture_2, 'Python', 2)
+student_2.rate_lec(lecture_2, 'Python', 2)
+
+print(student_1.grades)
+print(student_2.grades)
+print(lecture_1.grades)
+print(lecture_2.grades)
+
+print(reviewer_1)
+print(reviewer_2)
+print(lecture_1)
+print(student_1)
+print(student_2)
+
+print(student_1 < student_2)
+print(student_1 > student_2)
+
+print(lecture_1 < lecture_2)
+print(lecture_2 < lecture_1)

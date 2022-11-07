fec696eb63a4ea0584e235ea897a6499a02327c2
Yuri Shornikov <dbonlyfun@gmail.com>
2022-11-07 23:37:40 +0300

    1234-

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Student:
+    list_students = {}
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -6,7 +7,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        self.list_students = {}
+        # Student.list_students.append(self.name)
 
     def rate_lec(self, lecture, course, grade):
         if isinstance(lecture, Lecturer) and course in lecture.courses_attached and course in self.courses_in_progress:
@@ -16,6 +17,12 @@ class Student:
                 lecture.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    # def students_general(self, student, course):
+    #     if isinstance(student, Student) and course in student.courses_attached and course in self.courses_in_progress:
+    #         Student.list_students[self.name] += self.grades
+    #     else:
+    #         return 'Ошибка'
 
     def _average_rate(self):
         sum_grade = 0
@@ -39,7 +46,9 @@ class Student:
             return
         return self._average_rate() < other._average_rate()
 
-    def list_students(self):
+
+
+    # def list_students(self):
 
 class Mentor:
     def __init__(self, name, surname):
@@ -93,7 +102,7 @@ class Reviewer(Mentor):
         return f'Имя: {self.name}\n' \
                f'Фамилия: {self.surname}'
 
-def all_average_students(students, grades):
+# def all_average_students(students, courses):
 
 student_1 = Student('Ruoy', 'Eman', 'your_gender')
 student_1.courses_in_progress += ['Python']
@@ -101,6 +110,9 @@ student_1.courses_in_progress += ['C+']
 
 student_2 = Student('Jon', 'Sigal', 'man')
 student_2.courses_in_progress += ['Python']
+
+student_3 = Student('Mike', 'Magical', 'man')
+student_3.courses_in_progress += ['C+']
 
 reviewer_1 = Reviewer('Some', 'Buddy')
 reviewer_1.courses_attached += ['Python']
@@ -120,6 +132,9 @@ reviewer_1.rate_hw(student_1, 'Python', 10)
 reviewer_1.rate_hw(student_2, 'Python', 5)
 reviewer_1.rate_hw(student_2, 'Python', 3)
 
+reviewer_1.rate_hw(student_3, 'C+', 10)
+reviewer_1.rate_hw(student_3, 'C+', 8)
+
 student_1.rate_lec(lecture_1, 'C+', 10)
 student_1.rate_lec(lecture_1, 'C+', 5)
 student_1.rate_lec(lecture_1, 'C+', 0)
@@ -132,15 +147,23 @@ print(student_1.grades)
 print(student_2.grades)
 print(lecture_1.grades)
 print(lecture_2.grades)
+print(student_3.grades)
 
 print(reviewer_1)
 print(reviewer_2)
 print(lecture_1)
 print(student_1)
 print(student_2)
+print(student_3)
 
 print(student_1 < student_2)
 print(student_1 > student_2)
 
 print(lecture_1 < lecture_2)
 print(lecture_2 < lecture_1)
+
+print(Student.list_students)
+
+
+print(student_1.courses_in_progress)
+
